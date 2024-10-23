cd2c3d461733c2b97c01046b783ccf622cb76ab1
ayan3018 <ayan.pakhira3018@gmail.com>
2024-10-23 07:46:01 +0530

    project setup done completely

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, "./public/temp")
+    },
+    filename: function(req, file, cb) {
+       cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({storage})
+
+//basically the purpose of this is, before uploading the files on cloud we will pass them
+//through middleware which here is multer, then after uploading got completed we will
+//delete them from the middleware, to save them in the middleware we have used here
+//diskStorage for storage.
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,1 +1,49 @@
-import {v2} from "cloudinary"
+import { v2 as cloud } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        
+        //upload the file on the cloudinary
+       const response = await cloudinary.uploader.upload(localFilePath, 
+            {
+                resource_type:"auto"
+            })
+
+            //file has been successfully uploaded
+            console.log("file successfully uploaded on cloudinary", response.url)
+            return response
+
+    }catch(error){
+        fs.unlinkSync(localFilePath)
+        //this code will remove the locally saved temporary file as the upload 
+        //operation got failed.
+    }
+
+}
+
+export  {uploadOnCloudinary}
+ 
+
+    // // Upload an image
+    //  const uploadResult = await cloudinary.uploader
+    //    .upload(
+    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+    //            public_id: 'shoes',
+    //        }
+    //    )
+    //    .catch((error) => {
+    //        console.log(error);
+    //    });
+    
+    // console.log(uploadResult);
+    
+    
